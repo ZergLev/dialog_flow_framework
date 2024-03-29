c7296f4366a9abe248918bbc1b02e9b8d469cd2d
ZergLev <vasilev.sv@phystech.edu>
2024-03-29 08:05:04 +0500

    New declared path for switcher

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -147,7 +147,7 @@ html_theme_options = {
     ],
     "secondary_sidebar_items": ["page-toc", "source-links", "example-links"],
     "switcher": {
-        "json_url": "docs/build/feat/sphinx_multiversion/_static/switcher.json",
+        "json_url": "file:///home/sergey/DialogFlowFramework/SphinxMultiversioning/dialog_flow_framework/docs/build/feat/sphinx_multiversion/_static/switcher.json",
         "version_match": version,
     },
     "navbar_start": ["navbar-logo", "version-switcher"],
