86f7100909b5503e8354b836f0c1e32dac2acf8b
ZergLev <vasilev.sv@phystech.edu>
2024-04-09 18:49:20 +0500

    attempting linking fix

--- docs/source/utils/generate_tutorials.py
+++ docs/source/utils/generate_tutorials.py
@@ -98,9 +98,9 @@ def iterate_tutorials_dir_generating_links(source: Path, dest: Path, base: str) 
     links = list()
     for entity in [obj for obj in sort_tutorial_file_tree(set(source.glob("./*"))) if not obj.name.startswith("__")]:
         print(entity, "||", f"{base}")
-        print("My destination would be:", f"{base}/docs/source/tutorials.{entity.name}")
-        print("My destination would be:", f"{base.parent}.{dest}.{entity.name}")
-        print("My destination would be:", f"{base.parent}.{dest}.{entity.name}")
+        print("My destination would be:", f"{base}.{entity.name}")
+        print("My destination would be:", f"{base}.{dest}.{entity.name}")
+        print("My destination would be:", f"{base}.{dest}.{entity.name}")
         base_name = f"{base}.{entity.name}"
         print("source.name = ", f"{source.name}")
         print("base_name = ", base_name)
