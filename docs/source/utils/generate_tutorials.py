c392e394e956c0996d90663b7a5fe5d4f72c177b
ZergLev <vasilev.sv@phystech.edu>
2024-04-09 21:47:16 +0500

    attempting linking fix

--- docs/source/utils/generate_tutorials.py
+++ docs/source/utils/generate_tutorials.py
@@ -104,8 +104,8 @@ def iterate_tutorials_dir_generating_links(source: Path, dest: Path, base: str) 
     links = list()
     for entity in [obj for obj in sort_tutorial_file_tree(set(source.glob("./*"))) if not obj.name.startswith("__")]:
         print(entity, "||", f"{base}")
-        print("My destination would be:", f"{dest}/tutorials.{entity.name}")
-        base_name = f"{dest}/tutorials.{entity.name}"
+        print("My destination would be:", f"{base}.{entity.name}")
+        base_name = f"{base}.{entity.name}"
         print("source.name = ", f"{source.name}")
         print("base_name = ", base_name)
         if entity.is_file() and entity.suffix in (".py", ".ipynb"):
@@ -117,7 +117,8 @@ def iterate_tutorials_dir_generating_links(source: Path, dest: Path, base: str) 
             print(str(dest) / base_path)
             print(dest / str(base_path))
             print(Path(dest / str(base_path)))
-            create_notebook_link(entity, base_path)
+            dest_path = f"{base}"
+            create_notebook_link(entity, base_name)
             links += [base_path]
         elif entity.is_dir() and not entity.name.startswith("_"):
             links += iterate_tutorials_dir_generating_links(entity, dest, base_name)
@@ -155,7 +156,7 @@ def generate_tutorial_links_for_notebook_creation(
         else:
             flattened += [f"{package[0]}.{subpackage[0]}" for subpackage in package[2]]
 
-    links = iterate_tutorials_dir_generating_links(Path(source), dest, destination)
+    links = iterate_tutorials_dir_generating_links(Path(source), dest, f"{destination}.tutorials")
     filtered_links = list()
     for link in links:
         link_included = len(list(flat for flat in flattened if link.name.startswith(flat))) > 0
