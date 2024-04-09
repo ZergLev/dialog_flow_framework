f1630eefc3f74ce71a3a31f7e7faa5bbb714a9ba
ZergLev <vasilev.sv@phystech.edu>
2024-04-09 18:40:14 +0500

    attempting linking fix

--- docs/source/utils/generate_tutorials.py
+++ docs/source/utils/generate_tutorials.py
@@ -84,7 +84,7 @@ def sort_tutorial_file_tree(files: Set[Path]) -> List[Path]:
     return sorted(tutorials, key=sort_key) + sorted(files - tutorials)
 
 
-def iterate_tutorials_dir_generating_links(source: Path, dest: Path, base: str) -> List[Path]:
+def iterate_tutorials_dir_generating_links(source: Path, dest: Path, base: str, root_dir: str) -> List[Path]:
     """
     Recursively travel through tutorials directory, creating copies for all files under /tmp_dir/docs/source/tutorials/ root.
     Created copied files have absolute path name matching source file tree structure.
@@ -98,6 +98,7 @@ def iterate_tutorials_dir_generating_links(source: Path, dest: Path, base: str) 
     links = list()
     for entity in [obj for obj in sort_tutorial_file_tree(set(source.glob("./*"))) if not obj.name.startswith("__")]:
         print(entity, "||", f"{base}")
+        print("My destination would be:", f"{root_dir}/docs/source/tutorials.{entity.name}")
         print("My destination would be:", f"{base.parent}.{dest}.{entity.name}")
         base_name = f"{base}.{entity.name}"
         print("source.name = ", f"{source.name}")
@@ -123,7 +124,7 @@ def generate_tutorial_links_for_notebook_creation(
     exclude: Optional[List[str]] = None,
     source: str = "tutorials",
     destination: str = "docs/source/tutorials",
-    root_dir: str,
+    root_dir: str = ".",
 ):
     """
     Generate symbolic links to tutorials files (tutorials/) in docs directory (docs/source/tutorials/).
