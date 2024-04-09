6afa549d8cfe46d57ee860df00e24b2dd372c7ed
ZergLev <vasilev.sv@phystech.edu>
2024-04-09 19:23:07 +0500

    attempting linking fix

--- docs/source/utils/generate_tutorials.py
+++ docs/source/utils/generate_tutorials.py
@@ -16,7 +16,7 @@ def create_notebook_link(source: Path, destination: Path):
     # destination.symlink_to(source.resolve(), False)
     print("source = ", source.resolve())
     print("destination = ", destination)
-    result = copy(source.resolve(), Path('./folder/'))
+    result = copy(source.resolve(), destination.resolve())
     print(result)
     # Changing this into a create_notebook_copy, cause version-dependent source
     # files get deleted along with a temp directory during polyversion docs build.
